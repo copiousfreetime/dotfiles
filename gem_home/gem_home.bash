1041117ed581e804a2a8fa072b4e1a7b58145bbb
copiousfreetime <jeremy@copiousfreetime.org>
2018-03-07 15:23:15 -0700

    switch to full chruby integration

--- gem_home/gem_home.bash
+++ gem_home/gem_home.bash
@@ -1,3 +1,26 @@
-source ${DOTS}/gem_home/_gem_home
+if [[ "$(type -t gem_home)" != 'function' ]]; then
+  source ${DOTS}/gem_home/_gem_home
+fi
 
-alias gh='gem_home .'
+if [[ "$(type -t chruby)" != 'function' ]]; then
+  source ${DOTS}/chruby/chruby.bash
+fi
+
+
+function gh() {
+  local dir version
+  dir="$PWD/"
+  dir="${dir%/*}"
+
+  version_file="${dir}/.ruby-version"
+
+  if [[ -f "${version_file}" ]]; then
+    read -r version < "${version_file}"
+    echo "Using ruby version $version"
+    chruby ${version}
+  else
+    echo "$PWD/.ruby-version not found"
+  fi
+
+  gem_home .
+}
