9d7bea9a8bade5f7e7e63be38736e52233420c65
copiousfreetime <jeremy@copiousfreetime.org>
2023-11-07 11:56:57 -0700

    update fzf bash setup

--- bash/fzf.bash
+++ bash/fzf.bash
@@ -1,20 +1,13 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/opt/homebrew/opt/fzf/bin"
-fi
-
-# Man path
-# --------
-if [[ ! "$MANPATH" == */opt/homebrew/opt/fzf/man* && -d "/opt/homebrew/opt/fzf/man" ]]; then
-  export MANPATH="$MANPATH:/opt/homebrew/opt/fzf/man"
+  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.bash" 2> /dev/null
+source "/opt/homebrew/opt/fzf/shell/completion.bash"
 
 # Key bindings
 # ------------
 source "/opt/homebrew/opt/fzf/shell/key-bindings.bash"
-
