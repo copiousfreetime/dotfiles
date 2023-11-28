56c5aa4abae5274026cfeba46c111fc22e9d6d1c
copiousfreetime <jhinegardner@easypost.com>
2023-11-28 22:18:22 +0000

    live fixes

--- script/strap.nix
+++ script/strap.nix
@@ -7,20 +7,8 @@ set -e
 [[ "$1" == "--debug" || -o xtrace ]] && STRAP_DEBUG="1"
 STRAP_SUCCESS=""
 
-ask_for
-
-sudo_askpass() {
-  if [ -n "$SUDO_ASKPASS" ]; then
-    sudo --askpass "$@"
-  else
-    sudo "$@"
-  fi
-}
-
 cleanup() {
   set +e
-  sudo_askpass rm -rf "$CLT_PLACEHOLDER" "$SUDO_ASKPASS" "$SUDO_ASKPASS_DIR"
-  sudo --reset-timestamp
   if [ -z "$STRAP_SUCCESS" ]; then
     if [ -n "$STRAP_STEP" ]; then
       echo "!!! $STRAP_STEP FAILED" >&2
@@ -48,10 +36,6 @@ STDIN_FILE_DESCRIPTOR="0"
 [ -t "$STDIN_FILE_DESCRIPTOR" ] && STRAP_INTERACTIVE="1"
 
 
-# We want to always prompt for sudo password at least once rather than doing
-# root stuff unexpectedly.
-sudo --reset-timestamp
-
 # functions for turning off debug for use when handling the user password
 clear_debug() {
   set +x
@@ -63,62 +47,9 @@ reset_debug() {
   fi
 }
 
-# Initialise (or reinitialise) sudo to save unhelpful prompts later.
-sudo_init() {
-  if [ -z "$STRAP_INTERACTIVE" ]; then
-    return
-  fi
-
-  # If TouchID for sudo is setup: use that instead.
-  if grep -q pam_tid /etc/pam.d/sudo; then
-    return
-  fi
-
-  local SUDO_PASSWORD SUDO_PASSWORD_SCRIPT
-
-  if ! sudo --validate --non-interactive &>/dev/null; then
-    while true; do
-      read -rsp "--> Enter your password (for sudo access):" SUDO_PASSWORD
-      echo
-      if sudo --validate --stdin 2>/dev/null <<<"$SUDO_PASSWORD"; then
-        break
-      fi
-
-      unset SUDO_PASSWORD
-      echo "!!! Wrong password!" >&2
-    done
-
-    clear_debug
-    SUDO_PASSWORD_SCRIPT="$(cat <<BASH
-#!/bin/bash
-echo "$SUDO_PASSWORD"
-BASH
-)"
-    unset SUDO_PASSWORD
-    SUDO_ASKPASS_DIR="$(mktemp -d)"
-    SUDO_ASKPASS="$(mktemp "$SUDO_ASKPASS_DIR"/strap-askpass-XXXXXXXX)"
-    chmod 700 "$SUDO_ASKPASS_DIR" "$SUDO_ASKPASS"
-    bash -c "cat > '$SUDO_ASKPASS'" <<<"$SUDO_PASSWORD_SCRIPT"
-    unset SUDO_PASSWORD_SCRIPT
-    reset_debug
-
-    export SUDO_ASKPASS
-  fi
-}
-
-sudo_refresh() {
-  clear_debug
-  if [ -n "$SUDO_ASKPASS" ]; then
-    sudo --askpass --validate
-  else
-    sudo_init
-  fi
-  reset_debug
-}
-
 abort() { STRAP_STEP="";   echo "!!! $*" >&2; exit 1; }
-log()   { STRAP_STEP="$*"; sudo_refresh; echo "--> $*"; }
-logn()  { STRAP_STEP="$*"; sudo_refresh; printf -- "--> %s " "$*"; }
+log()   { STRAP_STEP="$*"; echo "--> $*"; }
+logn()  { STRAP_STEP="$*"; printf -- "--> %s " "$*"; }
 logk()  { STRAP_STEP="";   echo "OK"; }
 escape() {
   printf '%s' "${1//\'/\'}"
@@ -128,7 +59,7 @@ escape() {
 groups | grep $Q -E "\b(wheel)\b" || abort "Add $USER to the wheel group."
 
 # Setup Git configuration.
-logn "Configuring Git:"
+log "Configuring Git:"
 read -p "What is your git name: " STRAP_GIT_NAME
 read -p "What is your git email: " STRAP_GIT_EMAIL
 read -p "What is your github username: " STRAP_GITHUB_USER
@@ -173,12 +104,17 @@ fi
 
 ## Install all the dotfiles, base16 theme and vim
 log "Installing dotfiles, base16 theme and vim"
+rake=$(command -v rake)
+if [  -z $rake ]; then
+	gem install rake
+fi
 (
 cd ~/.dotfiles
 rake base16:install vim:install install
 )
 logk
 
+STRAP_SUCCESS="1"
 log "Now open up nvim and run :PlugInstall"
-logk
+log "Your system is now Strap'd!"
 
