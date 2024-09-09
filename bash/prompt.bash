3c94fab60152a989c7e27fd5bb22d945a7f06ef2
copiousfreetime <jhinegardner@easypost.com>
2024-09-09 14:45:49 -0600

    add theme

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -1,6 +1,7 @@
 
 #-- Colors --
 BASE16_SHELL="$HOME/.config/base16-shell/"
+BASE16_THEME=tomorrow-night
 [ -n "$PS1" ] && \
     [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
             eval "$("$BASE16_SHELL/profile_helper.sh")"
