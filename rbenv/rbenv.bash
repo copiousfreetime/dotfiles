e84d7a6070dee0595b44491188633cb4a045a703
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2024-10-26 13:25:51 -0600

    notify when using rbenv

--- rbenv/rbenv.bash
+++ rbenv/rbenv.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [ -d ~/.rbenv ]; then
+if [ -f ~/.rbenv/bin/rbenv ]; then
+  echo "Using rbenv"
   eval "$(~/.rbenv/bin/rbenv init - bash)"
-else
-  echo "ERROR: rbenv not found!"
-  echo "git clone https://github.com/rbenv/rbenv.git ~/.rbenv"
- # echo 'git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build'
+#else
+#  echo "ERROR: rbenv not found!"
+#  echo "git clone https://github.com/rbenv/rbenv.git ~/.rbenv"
+#  echo 'git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build'
 fi
 
