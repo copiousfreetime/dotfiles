7a9b8e6aaddc88c8ab02545370da0a01c0c84945
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2024-10-26 13:25:39 -0600

    silence the output of the brew path

--- bash/go.bash
+++ bash/go.bash
@@ -1,8 +1,8 @@
-
 # Setup Go
 export GOPATH=$HOME/go
 PATH="$PATH:$GOPATH/bin"
-if command -v brew; then
-  export GOROOT=$(brew --prefix go)/libexec
+if command -v brew > /dev/null; then
+  brew_prefix=$(brew --prefix go)
+  export GOROOT=${brew_prefix}/libexec
   PATH="$PATH:$GOROOT/bin"
 fi
