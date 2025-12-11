c2b10ae3daef23ff1c016aaa745d1be19d4198ec
Jeremy Hinegardner <jhinegardner@easypost.com>
2025-12-11 10:39:58 -0700

    set new default ruby

--- chruby/chruby.bash
+++ chruby/chruby.bash
@@ -1,5 +1,5 @@
 if [[ -f $HOMEBREW_ROOT/opt/chruby/share/chruby/chruby.sh ]]; then
   echo "Using chruby"
   source $HOMEBREW_ROOT/opt/chruby/share/chruby/chruby.sh
-  chruby 3.4.5
+  chruby 3.4.7
 fi
