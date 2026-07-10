55481f5bb57ab8d536a0c0b289087ed2142dd9ca
Jeremy Hinegardner <jhinegardner@easypost.com>
2026-07-10 15:25:28 +0000

    Restore chruby and rbenv hooks

---
+++ rbenv/rbenv.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -f ~/.rbenv/bin/rbenv ]; then
+  echo "Using rbenv"
+  eval "$(~/.rbenv/bin/rbenv init - bash)"
+#else
+#  echo "ERROR: rbenv not found!"
+#  echo "git clone https://github.com/rbenv/rbenv.git ~/.rbenv"
+#  echo 'git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build'
+fi
